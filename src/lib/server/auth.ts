ee315d62cfbb385408077b4e77ce3b6e2be37004
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2026-03-02 13:45:15 +0700

    feat: add user, session, account, and verification tables with relations for authentication

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -14,7 +14,11 @@ export const auth = betterAuth({
 		github: {
 			clientId: env.GITHUB_CLIENT_ID,
 			clientSecret: env.GITHUB_CLIENT_SECRET
-		}
+		},
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET
+		},
 	},
-	plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
+	plugins: [sveltekitCookies(getRequestEvent)]
 });
