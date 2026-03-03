6b555c24522fed0846478eb7340343528e19c11a
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2026-03-03 13:00:13 +0700

    Refactor code structure for improved readability and maintainability

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,32 @@
-import adapter from '@sveltejs/adapter-vercel';
-// import adapter from '@sveltejs/adapter-auto';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { createHighlighter } from 'shiki';
+
+const theme = 'github-dark';
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: ['javascript', 'typescript', 'css', 'html']
+});
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+			return `{@html \`${html}\` }`;
+		}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: { $components: 'src/components', $contents: 'src/contents' }
+	},
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
